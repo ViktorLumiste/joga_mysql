60981bdb1c1aaf768e2a93808bc05b32b6f99892
ViktorLumiste <viktor.lumiste@gmail.com>
2023-01-31 13:01:08 +0200

    Created basic project files

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+const path = require('path')
+
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"qwerty",
+    database: "joga_mysql"
 })
 
-app.listen(3000)
+con.connect(function(err){
+    if(err) throw err;
+    console.log("connected to joga_mysql db")
+})
+
+app.listen(3000, () =>{
+    console.log("App is started at http://localhost:3000")
+    })
