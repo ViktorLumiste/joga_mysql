d7f3957b762f9d00363fdb57fbb2bfced44e449d
ViktorLumiste <viktor.lumiste@gmail.com>
2023-01-31 13:51:37 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 
 const path = require('path')
 const hbs = require('express-handlebars')
-app.set('views', path.join(__dirname, 'views'));
-app.set('view-engine', 'hbs');
+app.set('views', path.join(__dirname, 'views/layouts'));
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname:'hbs',
-    defaultLayer: 'main',
-    layoutsDir: __dirname + 'views/layouts/',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
 }))
 
 app.use(express.static('public'))
@@ -22,12 +22,23 @@ var con = mysql.createConnection({
     host: "localhost",
     user:"root",
     password:"qwerty",
-    database: "joga_mysql"
+    database: "joga_mysql",
 })
 
 con.connect(function(err){
     if(err) throw err;
     console.log("connected to joga_mysql db")
+})
+app.get('/', (req,res) =>{
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err,result)=>{
+        if(err) throw err;
+        articles = result
+        res.render('index', {
+            articles:articles
+        })
+    })
 })
 
 app.listen(3000, () =>{
