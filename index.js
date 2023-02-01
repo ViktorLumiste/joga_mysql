75560fed8ae93841e8f9e7d6274446b08db0c49e
ViktorLumiste <viktor.lumiste@gmail.com>
2023-02-01 14:45:05 +0200

    Displayed author article list

--- index.js
+++ index.js
@@ -35,13 +35,14 @@ app.get('/', (req,res) =>{
     con.query(query, (err,result)=>{
         if(err) throw err;
         articles = result
+        console.log(articles)
         res.render('index', {
             articles:articles
         })
     })
 })
 
-app.use('/article/:slug', (req,res) =>{
+app.get('/article/:slug', (req,res) =>{
     let query = `SELECT *, article.name as article_name, author.name as author_name FROM article JOIN author on article.author_id = author.id WHERE slug="${req.params.slug}"`
     let article
     con.query(query, (err, result) =>{
@@ -53,7 +54,26 @@ app.use('/article/:slug', (req,res) =>{
         })
     })
 })
-
+app.get('/author/:author_ID', (req,res) =>{
+    let query = `SELECT * FROM author WHERE id="${req.params.author_ID}}"`
+    let author = []
+    con.query(query, (err, result) =>{
+        if(err) throw err;
+        author = result
+        console.log(author)
+        query = `SELECT * FROM article WHERE author_id="${req.params.author_ID}}"`
+        let articles = []
+        con.query(query, (err, result) => {
+            if (err) throw err;
+            articles = result
+            console.log(articles)
+            res.render('author', {
+                author: author,
+                articles: articles
+            })
+        })
+    })
+})
 app.listen(3000, () =>{
     console.log("App is started at http://localhost:3000")
     })
